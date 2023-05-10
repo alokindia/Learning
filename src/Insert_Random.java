1b3ca239b178f326fe7493e73e78ccc79624b019
alokindia <47476019+alokindia@users.noreply.github.com>
2023-05-10 20:54:49 +0530

    push

--- src/Insert_Random.java
+++ src/Insert_Random.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Insert_Random {
+    public static void main(String[] args){
+        int[] array = new int[10];
+        Scanner sc = new Scanner(System.in);
+        array[0]=4;
+        array[1]=7;
+        array[2]=9;
+        array[3]=2;
+        array[4]=6;
+        for(int i=0;i<array.length;i++)
+            System.out.println(array[i]);
+        System.out.println("Index you want to insert number:");
+        int index= sc.nextInt();
+        System.out.println("Enter the number:");
+        int number = sc.nextInt();
+        for(int j= array.length-1;j>=index;j--){
+            array[j+1]=array[j];
+        }
+        array[index]=number;
+        for(int i=0;i<array.length
+                ;i++)
+            System.out.println(array[i]);
+    }
 }
--- src/Linear_Search.java
+++ src/Linear_Search.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Linear_Search {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Linear_Search {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int []random= new int[10];
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+        System.out.println("Enter elements:");
+        for(int i=0;i<n;i++)
+            random[i]= sc.nextInt();
+        System.out.println("Enter element to be searched:");
+        int element = sc.nextInt();
+        for(int j=0;j<n;j++){
+            if(random[j]==element){
+                System.out.println("Found element at:"+(j+1));
+            }
+        }
+
+    }
 }
